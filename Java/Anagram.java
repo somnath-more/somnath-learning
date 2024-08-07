d29ba99e362b8d22a7f6702a215d0c53b07c7e65
Somnath Laxman More <somnath.more@zemosolabs.com>
2024-08-07 16:45:30 +0530

    📦 NEW:  Added Folder Structure for repo
    feat : Added Permissions
    
    Added Secrets and paths
    
    Added portfolio
    
    Added Context

---
+++ Java/Anagram.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class Anagram {
+    // Function to check if two strings are anagrams
+    public static boolean areAnagrams(String str1, String str2) {
+        // If lengths of both strings are not the same, they cannot be anagrams
+        if (str1.length() != str2.length()) {
+            return false;
+        }
+
+        // Convert strings to character arrays
+        char[] charArray1 = str1.toCharArray();
+        char[] charArray2 = str2.toCharArray();
+
+        // Sort the character arrays
+        Arrays.sort(charArray1);
+        Arrays.sort(charArray2);
+
+        // Compare the sorted arrays
+        return Arrays.equals(charArray1, charArray2);
+    }
+
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silenta";
+
+        if (areAnagrams(str1, str2)) {
+            System.out.println(str1 + " and " + str2 + " are anagrams.");
+        } else {
+            System.out.println(str1 + " and " + str2 + " are not anagrams.");
+        }
+    }
+}
---
+++ Portfolio/frontend-portfolio/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+           <h1>This is Somnath  More Portfolio Website </h1>
+    </div>
+  );
+}
+
+export default App;
