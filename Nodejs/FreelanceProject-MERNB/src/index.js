97554f6c48eaf007631f1855168a9b31315ecf6a
somnath-more <somnath.more@zemosolabs.com>
2024-09-23 10:55:04 +0530

    feat : Added Cicd workflow docker-compose

--- Nodejs/FreelanceProject-MERNB/src/index.js
+++ Nodejs/FreelanceProject-MERNB/src/index.js
@@ -26,7 +26,7 @@ app.set('views','views');
 // 
 
 dotenv.config()
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/website_tut')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://mongo/website_tut')
    .then(() => {
     console.log('Connected to MongoDB')
 
