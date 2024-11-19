78486088ebc019e12a26d763af063506ce18bcc1
somnath-more <somnath.more19@vit.edu>
2024-11-19 15:02:48 +0530

    fix : removed unused variable

--- Portfolio/frontend-portfolio/src/App.tsx
+++ Portfolio/frontend-portfolio/src/App.tsx
@@ -9,26 +9,22 @@ import ProjectPage from './components/pages/ProjectPage';
 import NotesPage from './components/pages/NotesPage';
 import AddNotesPage from './components/pages/AddNotePage';
 
-import "slick-carousel/slick/slick.css"; 
-import "slick-carousel/slick/slick-theme.css";
-import Carousel from './components/organisms/Home/SimpleSlider';
-// import SimpleSlider from './components/organisms/Home/SimpleSlider';
 
 function App() {
   return (
 
-    //  <Routes>
-    //    <Route path="/" element={<HomePage />} />
-    //    <Route path="/home" element={<HomePage />} />
-    //    <Route path="/contact" element={<ContactPage/>} />
-    //    <Route path="/about"   element={<AboutPage/>} />
-    //    <Route path="/skills"   element={<SkillPage/>} />
-    //    <Route path="/projects" element={<ProjectPage/>} />
-    //    <Route path="/notes" element={<NotesPage/>} />
-    //    <Route path="/add-notes" element={<AddNotesPage/>} />
+     <Routes>
+       <Route path="/" element={<HomePage />} />
+       <Route path="/home" element={<HomePage />} />
+       <Route path="/contact" element={<ContactPage/>} />
+       <Route path="/about"   element={<AboutPage/>} />
+       <Route path="/skills"   element={<SkillPage/>} />
+       <Route path="/projects" element={<ProjectPage/>} />
+       <Route path="/notes" element={<NotesPage/>} />
+       <Route path="/add-notes" element={<AddNotesPage/>} />
   
-    //  </Routes>
-    <Carousel/>
+     </Routes>
+
 
   );
 }
