abc78a5566f5a348b903a7e58b94379f921e2993
somnath-more <somnathmore8383@gmail.com>
2025-04-11 23:00:50 +0530

    NestedObject

--- react-daily/src/App.tsx
+++ react-daily/src/App.tsx
@@ -1,8 +1,8 @@
-
-import './App.css'
+import "./App.css";
+import NestedObject from "./components/organisms/NestedObject";
+// import Datatable from "./components/organisms/Pagination";
 // import LearningBootstrap from './components/organisms/LearningBootstrap';
-import SignUp from './components/organisms/LearningBootstrap/signup'
-import TicTacToe from './components/organisms/TicTacToe'
+// import TicTacToe from "./components/organisms/TicTacToe";
 // import SignInForm from './components/organisms/SignInForm'
 // import A from './components/hooks/UseContext/A'
 // import Rerender from './components/organisms/Rerender/Rerender'
@@ -11,28 +11,21 @@ import TicTacToe from './components/organisms/TicTacToe'
 // import CustomHook from './components/organisms/CustomHook';
 // import TodoList from './components/organisms/TodoList'
 
-
-
-
-
 function App() {
-
   return (
     <>
-     {/* <TodoList/> */}
-     {/* <CustomHook/> */}
-     {/* <Reducer/>
-     <UseMemo/> */}
-       {/* <Rerender/>
-        */}
-        {/* <SignInForm/> */}
-        {/* <LearningBootstrap/>
-         */}
-         {/* <SignUp/> */}
-         <TicTacToe/>
-     {/* <A/> */}
+      {/* <TodoList/> */}
+      {/* <CustomHook/> */}
+   
+      {/* <SignInForm/> */}
+      {/* <SignUp/> */}
+      {/* <TicTacToe/> */}
+      {/* <A/> */}
+      {/* <Datatable/>
+       */}
+       <NestedObject/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
